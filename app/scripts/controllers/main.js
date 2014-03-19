97dd663fe8d7f009e328d9d1aba531c70a2efbf1
x-bird <x-bird@qiubs.com>
2014-03-19 14:21:06 +0800

    初步完成

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('angularGeneratorApp')
   .controller('MainCtrl', function ($scope) {
+  	$scope.data = {};
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
